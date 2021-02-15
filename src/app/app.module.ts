046065c7467b056bedd3decc784b600427cdd5b8
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2021-02-14 18:51:07 -0600

    added a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [MainComponent],
+  bootstrap: [AppComponent, MainComponent]
 })
 export class AppModule { }
